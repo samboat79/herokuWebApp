f147c33df15a26d3fb5efde03831aa494f846b8e
sammybee04 <sbsamboat9@icloud.com>
2020-07-12 04:21:11 -0500

    commiting changes to movielisting files

--- movielistingapp/js/main.js
+++ movielistingapp/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   $("#searchForm").on("submit", function(e) {
+   $("#searchForm").on("keyup", function(e) {
        let searchText = $("#searchText").val();
        getMovies(searchText);
        
@@ -13,19 +13,18 @@ function getMovies(searchText){
         .then(function(response) {
             console.log(response);
             let movies = response.data.Search;
-            let output = " ";
+            let output = "";
 
             $.each(movies, function(index, movie){
                 output += `
                 <div class="col-md-3">
-                    <div class="card bg-light text-dark card-body mt-3 text center">
-                    </div>
+                    <div class="well text center">
                         <img src="${movie.Poster}">
-                        <h4>${movie.Title}</h4>
+                        <h5>${movie.Title}</h5 >
                         <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                    </div>
 
-
-                </div>
+    </div>
                 
                 `;
             });
@@ -53,13 +52,13 @@ function getMovie() {
         .then(function(response) {
             console.log(response);
             let movie = response.data;
-
+   
             let output = `
-                <div class="row">
+                <div class="row">  
                     <div class="col-md-4">
-                        <img src="${movie.Poster}" class="thumbnail"
+                        <img src="${movie.Poster}" class="thumbnail">
                     </div>
-                    <div class="col-md-8>
+                <div class="col-md-8">
                         <h2>${movie.Title}</h2>
                         <ul class="list-group">
                         <li class="list-group-item"><strong>Genre: </strong> ${movie.Genre}</li>
@@ -67,28 +66,23 @@ function getMovie() {
                         <li class="list-group-item"><strong>Rated: </strong> ${movie.Rated}</li>
                         <li class="list-group-item"><strong>IMDB Rating: </strong> ${movie.imdbRating}</li>
                         <li class="list-group-item"><strong>Director: </strong> ${movie.Director}</li>
-                        <li class="list-group-item"><strong>Writer: </strong> ${movie.Writer}</li>
+                        <li class="list-group-item"><strong>Writer: </ strong> ${movie.Writer}</li>
                         <li class="list-group-item"><strong>Actors: </strong> ${movie.Actors}</li>
                         </ul>
-                    
                     </div>
-
+                    </div>
                 </div>
 
                 <div class="row">
-                <div class="card bg-light text-dark card-body mt-3">
+                <div class="well">
                     <h3>Plot</h3>
                     ${movie.Plot}
                     <hr>
                     <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
                     
-                    <a href="index.html" class="btn btn-danger">Go Back To Search</a>
+                    <a href="index.html" class="btn btn-default">Go Back To Search</a>
                 </div>
-                
-                
-                </div>
-            
-         `;
+               </div> `;
 
          $("#movie").html(output);
 
